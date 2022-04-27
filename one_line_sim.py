d9a58d97a6f5b272e9fe606900a96daac5382079
GRELAM <grelam100@gmail.com>
2022-04-27 03:36:44 +0200

    Added possibility to create multiple lines on empty board

--- one_line_sim.py
+++ one_line_sim.py
@@ -6,6 +6,7 @@ from stop import Stop
 from line import Line
 import threading
 import time
+from tkinter.colorchooser import askcolor
 
 
 def get_params_for_sim():
@@ -30,8 +31,8 @@ class MainWindow:
         self.canvas.create_window(85, 80, window=self.entry_bus_frequency)
         self.start_button = Button(text='Start', command=self.initial_drawing)
         self.canvas.create_window(130, 110, window=self.start_button)
-        #TODO zmienić na None
-        self.lines = [Line('Zielona', 'green')]
+        self.lines = None
+        self.chosen_line = None
         self.previous_stop = None
 
     def start_simulation(self):
@@ -49,25 +50,43 @@ class MainWindow:
         self.canvas = Canvas(self.window, width=width, height=height, bg='black')
         self.canvas.pack(fill='both', expand=True)
         self.start_button = Button(text='Start', command=lambda: threading.Thread(target=self.start_simulation).start())
+        add_line_button = Button(text='Add line', command=self.add_line)
         self.canvas.create_window(1000, 1000, window=self.start_button)
+        self.canvas.create_window(980, 40, window=add_line_button)
         self.canvas.bind("<Button-1>", self.add_stop)
 
+    def add_line(self):
+        line_name_entry = Entry(self.window)
+        line_name_entry.bind('<Return>', lambda event: self.accept_line_name(event, line_name_entry))
+        self.canvas.create_window(500, 500, window=line_name_entry)
+
+    def accept_line_name(self, event, line_name_entry):
+        line_name = line_name_entry.get()
+        line_name_entry.destroy()
+        color = askcolor(title='Line color')
+        line = Line(line_name, color[1])
+        if self.lines is None:
+            self.lines = []
+        self.lines.append(line)
+        self.chosen_line = line
+
     def add_stop(self, event):
-        x = event.x
-        y = event.y
-        bus_stop_entry = Entry(self.window)
-        bus_stop_entry.bind('<Return>', lambda event: self.accept_name(event, bus_stop_entry, x, y))
-        self.canvas.create_window(x, y, window=bus_stop_entry)
+        if self.chosen_line is not None:
+            x = event.x
+            y = event.y
+            bus_stop_entry = Entry(self.window)
+            bus_stop_entry.bind('<Return>', lambda event: self.accept_name(event, bus_stop_entry, x, y))
+            self.canvas.create_window(x, y, window=bus_stop_entry)
 
     def accept_name(self, event, bus_stop_entry, x, y):
         bus_stop_name = bus_stop_entry.get()
         bus_stop_entry.destroy()
         added_stop = Stop(bus_stop_name, x, y)
-        self.lines[0].add_stop_to_line(added_stop)
+        self.chosen_line.add_stop_to_line(added_stop)
         self.canvas.create_text(x, y, fill='red', text=added_stop)
-        if self.previous_stop is not None:
+        if self.previous_stop is not None and self.previous_stop in self.chosen_line.stops:
             self.canvas.create_line(self.previous_stop.x_position, self.previous_stop.y_position, x,
-                                    y, fill=self.lines[0].color)
+                                    y, fill=self.chosen_line.color)
 
         self.previous_stop = added_stop
 
