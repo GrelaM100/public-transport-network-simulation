380c1ba93d44bf19b99a86e9f301c7a5d6e8447e
HannaTabaszewska <92364769+HannaTabaszewska@users.noreply.github.com>
2022-04-29 17:48:43 +0200

    #6 line changers for passengers
    Passengers decide to hop on and off buses based on their pre-planned itinerary

--- passenger.py
+++ passenger.py
@@ -1,8 +1,24 @@
 class Passenger:
-    def __init__(self, env, starting_stop, destination_stop):
+    def __init__(self, env, starting_stop, destination_stop, itinerary):
         self.env = env
         self.current_stop = starting_stop
         self.destination_stop = destination_stop
+        self.itinerary = itinerary
+        self.itinerary.remove(self.current_stop)
 
     def __str__(self):
-        return 'Pasażer jadący do ' + str(self.destination_stop) + ' obecnie w ' + str(self.current_stop)
+        return 'Pasażer jadący do ' + str(self.destination_stop) + ' przez ' + str(self.itinerary) \
+               + ' obecnie w ' + str(self.current_stop)
+
+    def decide_to_hop_off(self, current_stop, next_stop):
+        if current_stop == self.destination_stop or next_stop not in self.itinerary:
+            return True
+        return False
+
+    def decide_to_hop_on(self, next_stop):
+        if next_stop in self.itinerary:
+            return True
+        return False
+
+    def at_stop(self, stop):
+        self.itinerary.remove(stop)
