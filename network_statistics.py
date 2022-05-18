35560aed08886353bd5ba425b701e5465e362b12
GrelaM100 <51510707+GrelaM100@users.noreply.github.com>
2022-05-18 10:47:10 +0200

    Added feature to save statistics to csv file (#7)

--- network_statistics.py
+++ network_statistics.py
@@ -1,4 +1,5 @@
-import numpy as np
+import pandas as pd
+
 
 class Statistics:
     def __init__(self, bus_size):
@@ -6,6 +7,7 @@ class Statistics:
         self.buses_load = []
         self.commute_times = []
         self.buses_in_traffic = 0
+        self.statistics_table = None
 
     def register_data(self, type, data):
         if type == "bus":
@@ -16,6 +18,25 @@ class Statistics:
             self.buses_in_traffic += data
 
     def return_statistics(self):
-        return [{"Bus load" : sum(self.buses_load) / (self.bus_size * len(self.buses_load)),
-                 "Commuting time" :  sum(self.commute_times)  / len(self.commute_times),
-                 "Traffic delays" : self.buses_in_traffic}]
+        return [{"Bus load": sum(self.buses_load) / (self.bus_size * len(self.buses_load)),
+                 "Commuting time": sum(self.commute_times) / len(self.commute_times),
+                 "Traffic delays": self.buses_in_traffic}]
+
+    def update_table(self, time):
+        try:
+            data = {"Bus load": sum(self.buses_load) / (self.bus_size * len(self.buses_load)),
+                    "Commuting time": sum(self.commute_times) / len(self.commute_times),
+                    "Traffic delays": self.buses_in_traffic}
+        except ZeroDivisionError:
+            data = {"Bus load": sum(self.buses_load) / (self.bus_size * len(self.buses_load)),
+                    "Commuting time": 0,
+                    "Traffic delays": self.buses_in_traffic}
+
+        if self.statistics_table is None:
+            self.statistics_table = pd.DataFrame(data=data, index=[time])
+
+        else:
+            self.statistics_table = pd.concat([self.statistics_table, pd.DataFrame(data=data, index=[time])])
+
+    def save_data_to_csv(self):
+        self.statistics_table.to_csv('statistics.csv')
