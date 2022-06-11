513b63bbd56e940e796394417d16edd364af6199
grela <grelam100@gmail.com>
2022-06-11 20:47:49 +0200

    Added dynamic plotting and safe exiting

--- network_statistics.py
+++ network_statistics.py
@@ -11,6 +11,9 @@ class Statistics:
         self.commute_times = []
         self.buses_in_traffic = 0
         self.statistics_table = None
+        self.ax = None
+        self.canvas = None
+        self.indicator = 'Bus load'
 
     def register_data(self, type, data):
         if type == "bus":
@@ -44,31 +47,42 @@ class Statistics:
     def save_data_to_csv(self):
         self.statistics_table.to_csv('statistics.csv')
 
-    def plot_data(self, canvas, ax, indicator='Bus load'):
-        ax.clear()
-        ax.plot(self.statistics_table.index, self.statistics_table[indicator])
-        canvas.draw()
-        canvas.get_tk_widget().pack()
+    def plot_data(self, indicator='Bus load'):
+        self.ax.clear()
+        self.ax.plot(self.statistics_table.index, self.statistics_table[indicator])
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack()
 
-    def change_title_and_plot(self, window, canvas, ax, indicator):
-        window.title(indicator)
-        self.plot_data(canvas, ax, indicator)
+    def change_title_and_plot(self, window, indicator):
+        self.indicator = indicator
+        window.title(self.indicator)
+        self.plot_data(self.indicator)
+
+    def safe_window_close(self, window):
+        self.ax = None
+        self.canvas = None
+        window.destroy()
 
     def window_plot_data(self, root):
         new_window = Toplevel(root)
         new_window.title('Bus load')
         fig = plt.Figure(figsize=(10, 8), dpi=100)
-        ax = fig.add_subplot(111)
-        canvas = FigureCanvasTkAgg(fig, master=new_window)
+        self.ax = fig.add_subplot(111)
+        self.canvas = FigureCanvasTkAgg(fig, master=new_window)
+        new_window.protocol("WM_DELETE_WINDOW", lambda: self.safe_window_close(new_window))
         frame = Frame(new_window, bg='white')
         frame.pack(side=RIGHT, expand=True, fill=BOTH)
         bus_load_button = Button(frame, text='Bus load',
-                                 command=lambda: self.change_title_and_plot(new_window, canvas, ax, 'Bus load'))
+                                 command=lambda: self.change_title_and_plot(new_window, 'Bus load'))
         commute_times = Button(frame, text='Commute times',
-                               command=lambda: self.change_title_and_plot(new_window, canvas, ax, 'Commuting time'))
+                               command=lambda: self.change_title_and_plot(new_window, 'Commuting time'))
         buses_in_traffic_button = Button(frame, text='Traffic delays',
-                                         command=lambda: self.change_title_and_plot(new_window, canvas, ax, 'Traffic delays'))
+                                         command=lambda: self.change_title_and_plot(new_window, 'Traffic delays'))
         bus_load_button.pack(side=TOP)
         commute_times.pack(side=TOP)
         buses_in_traffic_button.pack(side=TOP)
-        self.plot_data(canvas, ax)
+        self.plot_data()
+
+    def update_plot(self):
+        if self.ax is not None and self.canvas is not None:
+            self.plot_data(self.indicator)
